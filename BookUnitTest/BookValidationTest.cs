7916fd26e2d189fa4bc31cc940fda6693910adfc
fbaun <fbaun@HIGHTOWER>
2023-09-19 09:13:23 +0200

    Add project files.

---
+++ BookUnitTest/BookValidationTest.cs
@@ -0,0 +1,71 @@
+using ObligatoriskOpg1Sem3;
+
+namespace BookUnitTest
+{
+    [TestClass]
+    public class BookValidationTest
+    {
+        [TestMethod]
+        public void ToStringTest()
+        {
+            //Arrange
+            Book book = new Book {Id = 1, Title = "Dracula", Price = 100 };
+
+            //Act
+            string result = book.ToString();
+
+            //Assert
+            Assert.AreEqual("1 Dracula 100", result);
+        }
+
+        [TestMethod]
+        public void ValidateTitleNullTest() 
+        {
+            //Arrange
+            Book book = new Book { Id = 1, Title = null, Price = 100 };
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => book.ValidateTitle());
+        }
+
+        [TestMethod]
+        public void ValidateTitleCharTest()
+        {
+            //Arrange
+            Book book = new Book {Id = 1, Title = "", Price = 100 };
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => book.ValidateTitle());
+        }
+
+        [TestMethod]
+        public void ValidatePriceTest()
+        {
+            //Arrange
+            Book book = new Book { Id = 1, Title = "Dracula", Price = 0 };
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => book.ValidatePrice());
+        }
+
+        [TestMethod]
+        public void ValidateTitleMethodTest() 
+        {
+            //Arrange
+            Book book = new Book {Id = 1, Title = "", Price = 100 };
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => book.Validate());
+        }
+
+        [TestMethod]
+        public void ValidatePriceMethodTest()
+        {
+            //Arrange
+            Book book = new Book {Id = 1, Title = "Dracula", Price = 0 };
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => book.Validate());
+        }
+    }
+}
---
+++ ObligatoriskOpg1Sem3/Book.cs
@@ -0,0 +1,74 @@
+﻿namespace ObligatoriskOpg1Sem3
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+        public int Price { get; set; }
+
+        //Properties with buildin non-specific exception handling  
+        //public string Title {
+        //    get { return Title; }
+        //    set {
+        //        if (value != null && value.Length >= 3)
+        //        {
+        //            Title = value;
+        //        }
+        //        else {
+        //            throw new ArgumentException("Title must be a non-null string and more than 3 chars in length.");
+        //        }
+        //    } 
+        //}
+        //public int Price {
+        //    get { return Price; }
+        //    set
+        //    {
+        //        if (value > 0 && value <= 1200)
+        //        {
+        //            Price = value;
+        //        }
+        //        else
+        //        {
+        //            throw new ArgumentException("Price can't be less than 0 or higher than 1200.");
+        //        }
+        //    }
+        //}
+
+
+        public override string ToString()
+        {
+            return $"{Id} {Title} {Price}";
+        }
+
+
+        public void ValidateTitle() 
+        {
+            if (Title == null)
+            {
+                throw new ArgumentNullException("Title can't be null.");
+            }
+
+            if(Title.Length < 3)
+            {
+                throw new ArgumentException("Title must be more than 3 chars.");
+            }
+        
+        }
+
+        public void ValidatePrice()
+        {
+            if(Price <= 0 || Price > 1200) 
+            {
+                throw new ArgumentOutOfRangeException("Price is not within the range of 0 < Price <= 1200.");
+            }
+
+        }
+
+        public void Validate() 
+        {
+            ValidateTitle();
+            ValidatePrice();
+        }
+    }
+}
